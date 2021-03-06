7ca7814a34879bcde9c4b24ad13cb55351ebb958
paulahemsi <paulahemsi@gmail.com>
2021-03-05 21:55:02 -0300

    flags struct

---
+++ ft_printf.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 20:26:47 by phemsi-a          #+#    #+#             */
+/*   Updated: 2021/03/05 21:32:21 by phemsi-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+
+# define TRUE	1
+# define FALSE	0
+
+typedef	struct	ft_printf
+{
+	int	left_align;
+	int	zero_padding;
+	int	min_width;
+	int	precision;	
+}				flags;
+
+int	ft_printf(const char *str, ...);
+
+#endif
