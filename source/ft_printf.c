0f5810ff241b86ecd01f739c69041a49ade4a2a3
paulahemsi <paulahemsi@gmail.com>
2021-03-06 14:04:01 -0300

    com main p testes iniciais

--- source/ft_printf.c
+++ source/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 21:16:09 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/03/06 13:01:38 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/03/06 13:49:18 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,16 @@ static int	find_length(char *str, char *pointer)
 	while (str[length] != pointer[0])
 		length++;
 	return (length);
+}
+
+static int update_str(char **str_return, char **str_temp, char **str_aux)
+{
+	free(*str_return);
+	free(*str_aux);
+	if (*str_temp == NULL)
+		return (ERROR);
+	*str_return = *str_temp;
+	return (0);
 }
 
 static char	*flags_parser(char **pointer, va_list args)
@@ -45,17 +55,15 @@ static char	*flags_parser(char **pointer, va_list args)
 		initial_ptr++;
 	}
 	*pointer = initial_ptr;
-	ft_putstr("PONTEIRO: ");
-	ft_putchar(**pointer);
-	ft_putchar('\n');
 	return (parsed_str);
 }
 
-static void	ft_printf_recursive(char *str, va_list args)
+static int	ft_printf_recursive(char *str, va_list args)
 {
 	int				length;
 	char			*pointer;
 	char			*str_temp;
+	char			*str_aux;
 	static char		*str_return;
 
 	if (str_return == NULL)
@@ -66,23 +74,23 @@ static void	ft_printf_recursive(char *str, va_list args)
 		free(str_return);
 		str_return = str_temp;
 		ft_putstr(str_return);
+		free(str_return);
+		return (1);
 	}
 	else
 	{
 		length = find_length(str, pointer);
-		str_temp = ft_strjoin(str_return, ft_substr(str, 0, length)); //!lidar com NULL
-		free(str_return);
-		str_return = str_temp;
-		ft_putstr("JOIN COM PARTES ANTES DA %: ");
-		ft_putstr(str_return);
-		ft_putchar('\n');
-		str_temp =ft_strjoin(str_return, flags_parser(&pointer, args)); //!lidar com NULL e leak do parser
-		free (str_return);
-		str_return = str_temp;
-		ft_putstr("em processo: ");
-		ft_putstr(str_return);
-		ft_putchar('\n');
-		ft_printf_recursive(pointer, args);
+		if (!(str_aux = ft_substr(str, 0, length)))
+			return (ERROR);
+		str_temp = ft_strjoin(str_return, str_aux);
+		if (update_str(&str_return, &str_temp, &str_aux) == ERROR)
+			return (ERROR);
+		if (!(str_aux = flags_parser(&pointer, args)))
+			return (ERROR);
+		str_temp =ft_strjoin(str_return, str_aux);
+		if (update_str(&str_return, &str_temp, &str_aux) == ERROR)
+			return (ERROR);
+		return (ft_printf_recursive(pointer, args));
 	}
 }
 
@@ -91,12 +99,13 @@ int			ft_printf(const char *str, ...)
 	va_list	args;
 
 	va_start(args, str);
-	ft_printf_recursive((char *)str,args);
+	if (ft_printf_recursive((char *)str,args) == ERROR)
+		return (ERROR);
 	va_end(args);
 	return (0);
 }
 
 int main(void)
 {
-	ft_printf("uma string com uma porcentagem %% e um número(42) %i", 42);
+	ft_printf("uma string com uma porcentagem %% e um número(42) %i e outro número (dessa vez 1048) %i e vamos mais um(1234567) %i", 42, 1048, 1234567);
 }
