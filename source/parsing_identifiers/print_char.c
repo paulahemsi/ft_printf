b9d41ba924db4217e734e12b8911007d230ffaf6
paulahemsi <paulahemsi@gmail.com>
2021-03-07 21:10:01 -0300

    print char

---
+++ source/parsing_identifiers/print_char.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_char.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/07 18:18:16 by phemsi-a          #+#    #+#             */
+/*   Updated: 2021/03/07 21:03:55 by phemsi-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../ft_printf.h"
+
+void	print_char(flags *flag, va_list args)
+{
+	if (flag->left_align == TRUE)
+	{
+		ft_putchar((char)va_arg(args, int));
+		flag->length++;
+	}
+	while (flag->min_width > 0)
+	{
+		ft_putchar(' ');
+		flag->length++;
+		flag->min_width--;
+	}
+	if (flag->left_align == FALSE)
+	{	
+		ft_putchar((char)va_arg(args, int));
+		flag->length++;
+	}
+}
