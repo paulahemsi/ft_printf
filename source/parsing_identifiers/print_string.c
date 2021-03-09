83bd48da9fbac0829171e9f177adbca382071c31
paulahemsi <paulahemsi@gmail.com>
2021-03-09 10:41:00 -0300

    width corrigida

--- source/parsing_identifiers/print_string.c
+++ source/parsing_identifiers/print_string.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 14:49:08 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/03/08 15:49:33 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/03/09 10:39:40 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,23 @@
 void	print_string(flags *flag, va_list args)
 {
 	char	*string;
+	size_t	length;
 
 	if (flag->precision)
 		string = ft_substr(va_arg(args, char *), 0, flag->precision);
 	else
 		string = ft_strdup(va_arg(args, char *));
+	length = ft_strlen(string);
 	if (flag->left_align)
-	{
 		ft_putstr(string);
-		flag->length += ft_strlen(string);
-	}
-	while (flag->min_width > 0)
+	while (flag->min_width > length)
 	{
 		ft_putchar(' ');
 		flag->length++;
 		flag->min_width--;
 	}
 	if (!(flag->left_align))
-	{
 		ft_putstr(string);
-		flag->length += ft_strlen(string);
-	}
+	flag->length += ft_strlen(string);
 	free(string);
 }
