641cb418343fb9aac06a0e30f99f4ca496836899
paulahemsi <paulahemsi@gmail.com>
2021-03-12 14:00:19 -0300

    print hex

--- source/flags_parser.c
+++ source/flags_parser.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 15:11:09 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/03/11 22:09:20 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/03/12 13:41:59 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 static void	identifier_parser(flags *flag, va_list args)
 {
-	if (*flag->ptr == 'c')
+	if (*flag->ptr == '%')
+	{
+		ft_putchar('%');
+		flag->length++;
+	}
+	else if (*flag->ptr == 'c')
 		print_char(flag, args);
 	else if (*flag->ptr == 's')
 		print_string(flag, args);
@@ -24,8 +29,8 @@ static void	identifier_parser(flags *flag, va_list args)
 		print_unsigned_int(flag, args);
 	else if (*flag->ptr == 'p')
 		print_pointer(flag, args);
-	// else if (*flag->ptr == 'x' || *flag->ptr == 'X')
-	// 	print_hex(flag, args);
+	else if (*flag->ptr == 'x' || *flag->ptr == 'X')
+		print_hex(flag, args);
 }
 
 static void	update_width(flags *flag, va_list args)
@@ -73,24 +78,15 @@ size_t		flags_parser(char **ptr, va_list args, size_t length)
 	ft_memset(&flag, 0, sizeof(flag));
 	flag.length = length;
 	flag.ptr = *ptr + 1;
-	if (*flag.ptr == '%')
-	{
-		ft_putchar('%');
-		flag.length++;
-		flag.ptr++;
-	}
-	else
-	{
-		while (ft_strchr("-0", *flag.ptr))
-			update_padding(&flag);
-		if (ft_isdigit(*flag.ptr) || *flag.ptr == '*')
-			update_width(&flag, args);
-		if (*flag.ptr == '.')
-			update_precision(&flag, args);
-		if (ft_isalpha(*flag.ptr))
-			identifier_parser(&flag, args);
-		flag.ptr++;
-	}
+	while (ft_strchr("-0", *flag.ptr))
+		update_padding(&flag);
+	if (ft_isdigit(*flag.ptr) || *flag.ptr == '*')
+		update_width(&flag, args);
+	if (*flag.ptr == '.')
+		update_precision(&flag, args);
+	if ((ft_isalpha(*flag.ptr)) || *flag.ptr == '%')
+		identifier_parser(&flag, args);
+	flag.ptr++;
 	*ptr = flag.ptr;
 	return (flag.length);
 }
