ad770d98d8277339ffbd6ddb0f0c9d5384ae3dd6
paulahemsi <paulahemsi@gmail.com>
2021-03-12 19:05:41 -0300

    %% com 0

--- source/parsing_identifiers/print_char.c
+++ source/parsing_identifiers/print_char.c
@@ -6,34 +6,46 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 18:18:16 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/03/12 14:12:57 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/03/12 19:03:38 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../ft_printf.h"
 
+static void	print_left_align(flags *flag, va_list args)
+{
+	if (*flag->ptr == '%')
+		ft_putchar(*flag->ptr);
+	else
+		ft_putchar((char)va_arg(args, int));
+	flag->length++;
+}
+
+static void	print_right_align(flags *flag, va_list args)
+{
+	if (*flag->ptr == '%')
+		ft_putchar(*flag->ptr);
+	else
+		ft_putchar((char)va_arg(args, int));
+	flag->length++;
+}
+
 void	print_char(flags *flag, va_list args)
 {
+	char c;
+
+	if ((flag->zero_padding) && (*flag->ptr == '%'))
+		c = '0';
+	else
+		c = ' ';
 	if (flag->left_align)
-	{
-		if (*flag->ptr == '%')
-			ft_putchar(*flag->ptr);
-		else
-			ft_putchar((char)va_arg(args, int));
-		flag->length++;
-	}
+		print_left_align(flag, args);
 	while (flag->min_width > 1)
 	{
-		ft_putchar(' ');
+		ft_putchar(c);
 		flag->length++;
 		flag->min_width--;
 	}
 	if (!(flag->left_align))
-	{	
-		if (*flag->ptr == '%')
-			ft_putchar(*flag->ptr);
-		else
-			ft_putchar((char)va_arg(args, int));
-		flag->length++;
-	}
+		print_right_align(flag, args);
 }
