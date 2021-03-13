eec29c01799d8d512f108003693b77a856bbf772
paulahemsi <paulahemsi@gmail.com>
2021-03-12 23:54:19 -0300

    resolvendo detalhes da %u

--- source/parsing_identifiers/print_unsigned_int.c
+++ source/parsing_identifiers/print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 22:52:22 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/03/10 23:09:31 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/03/12 23:51:41 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,14 +40,16 @@ static void	print_width(flags *flag, int num_len)
 	char	c;
 	int		limit;
 
-	if ((flag->zero_padding) && !(flag->precision) && !(flag->left_align))
+	if ((flag->zero_padding) && (flag->precision != 0) && !(flag->left_align))
 		c = '0';
 	else
 		c = ' ';
 	if (flag->precision > num_len)
 		limit = flag->precision;
-	else
+	else if (flag->precision != 0)
 		limit = num_len;
+	else
+		limit = 0;
 	while (flag->min_width > limit)
 	{
 		ft_putchar(c);
@@ -66,14 +68,16 @@ void		print_unsigned_int(flags *flag, va_list args)
 	if (flag->left_align)
 	{
 		print_precision(flag, (flag->precision - num_len));
-		ft_putnbr_unsigned(number);
+		if (flag->precision != 0)
+			ft_putnbr_unsigned(number);
 		print_width(flag, num_len);
 	}
 	else
 	{
 		print_width(flag, num_len);
 		print_precision(flag, (flag->precision - num_len));
-		ft_putnbr_unsigned(number);
+		if (flag->precision != 0)
+			ft_putnbr_unsigned(number);
 	}
 	flag->length += num_len;
 }
