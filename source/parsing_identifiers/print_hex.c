4bf5c0f15d9c297b24899e0ad0e8c65474fa66ee
paulahemsi <paulahemsi@gmail.com>
2021-03-13 16:16:32 -0300

    hex done

--- source/parsing_identifiers/print_hex.c
+++ source/parsing_identifiers/print_hex.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 23:34:03 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/03/13 16:05:51 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/03/13 16:11:22 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,18 @@ static void	print_precision(flags *flag, int precision)
 	}
 }
 
-static void	print_width(flags *flag, int hex_len)
+static void	print_width(flags *flag, int hex_len, char *hex)
 {
 	char	c;
 	int		limit;
 
-	if ((flag->zero_padding) && (flag->precision == -1) && !(flag->left_align))
+	if ((flag->zero_padding) && (flag->precision < 0) && !(flag->left_align))
 		c = '0';
 	else
 		c = ' ';
 	if (flag->precision > hex_len)
 		limit = flag->precision;
-	else if (flag->precision != 0)
+	else if ((flag->precision) || (*hex != '0'))
 		limit = hex_len;
 	else
 		limit = 0;
@@ -54,12 +54,12 @@ void		print_hex(flags *flag, va_list args)
 	hex = ft_itoa_hex(va_arg(args, unsigned int), *flag->ptr);
 	hex_len = ft_strlen(hex);
 	if (!(flag->left_align))
-		print_width(flag, hex_len);
+		print_width(flag, hex_len, hex);
 	print_precision(flag, (flag->precision - hex_len));
 	if (*hex != '0' || flag->precision != 0)
 		ft_putstr(hex);
 	if (flag->left_align)
-		print_width(flag, hex_len);
+		print_width(flag, hex_len, hex);
 	free(hex);
 	flag->length += hex_len;
 }
